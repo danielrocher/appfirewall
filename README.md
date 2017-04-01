801be55a14d80aeab314eb01cb0689a66ccc253d
Daniel Rocher <daniel.rocher@resydev.fr>
2017-04-01 16:29:46 +0200

    added queueasyncthread.py (use libnetfilter_queue) added .gitignore

--- README.md
+++ README.md
@@ -1,2 +1,70 @@
-# appfirewall
-Monitor or block outbound traffic (filtered by process). Experimental
+# Appfirewall
+Monitor or block outbound traffic (filtered by process). __Experimental__
+
+_This program is licensed to you under the terms of the GNU General Public License version 3_
+
+# Installation
+
+
+## Module
+
+Appfirewall require NFQUEUE. It's an iptables and ip6tables target which delegate the decision on packets to a userspace software.
+
+```bash
+modprobe nfnetlink_queue
+```
+
+Check :
+
+```bash
+lsmod | grep queue
+nfnetlink_queue        20480  1
+```
+
+
+# Usage
+
+It is necessary to insert iptables rules to send traffic to a queue (by default, queue 0).
+
+Examples:
+```bash
+iptables  -I OUTPUT ! -o lo -j NFQUEUE --queue-bypass
+ip6tables -I OUTPUT ! -o lo -j NFQUEUE --queue-bypass
+```
+ * `--queue-num <number>` : Queue number
+ * `--queue-bypass` : The packet are authorized if no software is listening to the queue
+
+
+# Debugging
+
+## To monitor the status of _libnetfilter_queue_ :
+```bash
+watch -n 5 cat /proc/net/netfilter/nfnetlink_queue
+1  31621     0 2  4016     0     0        2  1
+```
+
+* Queue number
+* Process ID: process ID of program listening to the queue
+* Queue total: current number of packets waiting in the queue
+* Copy mode
+* Copy size
+* Queue dropped: number of packets dropped because queue was full
+* User dropped: number of packets dropped because netlink message could not be sent to userspace.
+* Total number of packets sent to queue
+* 1
+
+## Test _libnetfilter_queue_ :
+
+In userspace, _queueasyncthread.py_ use libnetfilter_queue (queue 0)
+
+```bash
+./appfw/queueasyncthread.py 
+Setting callback
+Open nfqueue number 0
+Queue is ready
+Quit with CTRL+C
+ 17 | 192.168.1.12 > 192.168.1.254
+ 6 | 192.168.1.12 > 217.160.231.227
+ 1 | 192.168.1.12 > 192.168.58.1
+```
+
---
+++ appfw/queueasyncthread.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+
+# Written by Daniel Rocher <erable@resydev.fr>
+# Portions created by the Initial Developer are Copyright (C) 2016
+
+
+from socket import AF_INET, AF_INET6, inet_ntop
+import nfqueue
+import asyncore
+from threading import Thread
+
+
+class QueueAsyncThread(asyncore.file_dispatcher, Thread):
+    def __init__(self, callback, queue_number=0, debug=False):
+        Thread.__init__(self)
+        self.debug=debug
+        self._stop = False
+        self.nf_queue_started=False
+        self.queue_number=queue_number
+        self.q = nfqueue.queue()
+        self.printDebug("Setting callback")
+        self.q.set_callback(callback)
+        self.printDebug("Open nfqueue number %s" % self.queue_number)
+        try:
+            self.q.fast_open(self.queue_number, AF_INET6)
+            self.fd = self.q.get_fd()
+            self.q.set_queue_maxlen(100000)
+            asyncore.file_dispatcher.__init__(self, self.fd, None)
+            self.q.set_mode(nfqueue.NFQNL_COPY_PACKET)
+        except:
+            self._stop = True
+            print "Impossible to open NetFilter Queue {}".format(self.queue_number)
+            return
+
+        self.nf_queue_started=True
+        self.printDebug("Queue is ready")
+
+    def printDebug(self, msg):
+        if self.debug:
+            print(str(msg))
+
+    def handle_read(self):
+        self.q.process_pending(5)
+    
+    def writable(self):
+        return False
+    
+    def run(self):
+        while not self._stop:
+            asyncore.poll(timeout=0.1)
+        
+        if self.nf_queue_started:
+            self.printDebug("Stopping QueueAsyncThread ...")
+            self.printDebug("Unbind nf_queue")
+            try:
+                self.q.unbind(AF_INET6)
+                self.printDebug("Close nf_queue {}".format(self.queue_number))
+                self.q.close()
+            except:
+                pass
+            self.nf_queue_started=False
+            self.printDebug("QueueAsyncThread stopped")
+
+    def stop(self):
+        self._stop=True
+
+
+if __name__ == '__main__':
+    import signal, time
+    from dpkt import ip, ip6
+    global thread
+    
+    def signal_handler(signal, frame):
+        print "Wait. Stopping all ..."
+        global thread
+        thread.stop()
+        thread.join()
+        thread=None
+
+    def callback(i, payload):
+        protocol=None
+        ipsource=None
+        ipdestination=None
+        data = payload.get_data()
+        try: # try IPv4
+            pkt = ip.IP(data)
+            protocol = pkt.p
+            ipsource=inet_ntop(AF_INET, pkt.src)
+            ipdestination=inet_ntop(AF_INET, pkt.dst)
+        except:
+            try: # try IPv6
+                pkt = ip6.IP6(data)
+                protocol = pkt.nxt
+                ipsource="%s" % inet_ntop(AF_INET6, pkt.src)
+                ipdestination="%s" % inet_ntop(AF_INET6, pkt.dst)
+            except:
+                print "Impossible to unpack IP"
+
+        print " %s | %s > %s" % (protocol, ipsource, ipdestination)
+        payload.set_verdict(nfqueue.NF_ACCEPT)
+
+    thread = QueueAsyncThread(callback, 0, debug=True)
+    print "Quit with CTRL+C"
+    
+    signal.signal(signal.SIGINT, signal_handler)
+    thread.start()
+
+    while thread:
+       time.sleep(1)
+
+
+        
